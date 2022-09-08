55ef348de4b84b61c38a1f2929c94ce3be63b624
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-08 09:15:44 -0700

    added the 6-size file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return: 0 succeful
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf"Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
