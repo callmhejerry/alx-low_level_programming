6e1da6a60b25906c4bc1cfce20c32f12bc751c88
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-10 07:32:30 -0700

    hot fixx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,73 +1,38 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * * main - loop through variables a,b,c printing digits
- *
- * *
- *
- * * Return: print to stdout all possible different combinations of three digits
- *
- * **/
-
+ * main - print the combination
+ * of two bas10 digits
+ * Return: 0 successful
+ */
 int main(void)
-
 {
-
-	int a = 0;
-
+	int a;
 	int b;
 
-
-
+	a = 0;
 	while (a <= 98)
-
 	{
-
 		b = a + 1;
-
 		while (b <= 99)
-
 		{
-
 			putchar(a / 10 % 10 + '0');
-
 			putchar(a % 10 + '0');
-
 			putchar(' ');
-
 			putchar(b / 10 % 10 + '0');
-
 			putchar(b % 10 + '0');
-
 			if (a == 98 && b == 99)
-
 			{
-
 				putchar('\n');
-
 			}
-
 			else
-
 			{
-
 				putchar(',');
-
 				putchar(' ');
-
 			}
-
 			b++;
-
 		}
-
 		a++;
-
 	}
-
 	return (0);
-
 }
