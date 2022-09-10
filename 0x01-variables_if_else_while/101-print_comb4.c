28c6c808742b1534037df74d9bc9ebd2902f80c0
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-09 23:15:40 -0700

    added 101 file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all unique possible
+ * digit of a base10 number
+ * Return: 0 successfuk
+ */
+int main(void)
+{
+	int numa;
+	int numb;
+	int numc;
+
+	numa = 48;
+	numb = 48;
+	numc = 48;
+	while (numa < 57)
+	{
+		if (numc > numb > numa)
+		{
+			putchar(numa);
+			putchar(numb);
+			putchar(numc);
+			if (numb + 1 == 57)
+				putchar('\n');
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		if (numc == 57)
+		{
+			numb++;
+			numc = 48;
+		}
+		if (numb == 57)
+			numa++;
+		numc++;
+	}
+	return (0);
+}
