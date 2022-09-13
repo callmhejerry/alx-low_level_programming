2a50a0fb91a5b2b90a635579ff446672ad943ef5
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-13 07:35:05 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - gives the absolute valueof a number
+ * @n: the number to be tested on 
+ *
+ * Return: the positive of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
