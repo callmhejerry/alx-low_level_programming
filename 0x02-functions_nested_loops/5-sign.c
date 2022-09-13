2cf954cb6e7d4a4e7bccbcb6de6964efc5dc9539
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-13 07:18:06 -0700

    added 5-sign file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - checks if a number is negative
+ * positive
+ * @n: the number to be checked 
+ *
+ * Return: 1 for positive number,
+ * 0 for a zero number , -1 for a negative number
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
