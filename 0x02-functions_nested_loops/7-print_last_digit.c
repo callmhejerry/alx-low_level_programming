87091a59f87343896bab6c2eb6c7bbcb6847a358
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-13 08:04:14 -0700

    added 7-print_last_digit file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,16 @@ int print_last_digit(int n)
 {
 	int last_num;
 
-	last_num = n % 10;
-	_putchar(last_num + '0');
-	return (last_num);
+	if (n < 0)
+	{
+		last_num = -1 * (n % 10);
+		_putchar(last_num + '0');
+		return (last_num);
+	}
+	else
+	{
+		last_num = n % 10;
+		_putchar(last_num + '0');
+		return (last_num);
+	}
 }
