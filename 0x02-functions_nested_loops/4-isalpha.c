fed0acb8d65cf63cbd242f267cc6a2158728f2e0
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-13 06:52:31 -0700

    added 4-isalpha file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _isalpha - checks if a character is
+ * an alphabet
+ * @c: the character to check
+ *
+ * Return: 1 if character is alpha,
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'B'))
+		return (1);
+	else
+		return (0);
+}
