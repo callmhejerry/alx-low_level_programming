7b808e4963ff0e952e5791b71d8de76bba994a6c
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-14 15:42:13 -0700

    added 103 file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the
+ * even numbers of a fibonacci sequence
+ * Return: 0 success
+ */
+int main(void)
+{
+	long a;
+	long b;
+	long c;
+	long sum;
+
+	sum = 2;
+	c = 3;
+	a = 2;
+	b = 3;
+	while (c <= 4000000)
+	{
+		if (c % 2 == 0)
+			sum += c;
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return (0);
+}
