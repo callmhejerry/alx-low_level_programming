ce27313f2447596d172a1cd3c17d5285db5357c4
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-14 07:58:23 -0700

    added print_to_98 file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -49,14 +49,14 @@ void less_than_98(int n)
 			_putchar('-');
 			if (n < -99)
 			{
-				_putchar((n / 10) / 10 + '0');
-				_putchar((n / 10) % 10 + '0');
-				_putchar(n % 10 + '0');
+				_putchar((-n / 10) / 10 + '0');
+				_putchar((-n / 10) % 10 + '0');
+				_putchar(-n % 10 + '0');
 			}
 			else if (n < -9)
 			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
+				_putchar(-n / 10 + '0');
+				_putchar(-n % 10 + '0');
 			}
 			else
 				_putchar((-1 * n) + '0');
