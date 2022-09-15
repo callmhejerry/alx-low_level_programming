0131bd0f4aee79a74e3ac40aca76307d22bd2e3a
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-15 04:59:04 -0700

    added 9-fizz-buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100
+ * , but prints Fizz when the number is divisible
+ * by 3 , Buzz when it is divisible by 5 and
+ * FizzBuzz when it is divisible by 15
+ * Return: 0n success
+ */
+int main(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", i);
+		if (i == 100)
+			printf("\n");
+		else
+			printf(" ");
+	}
+	return (0);
+}
