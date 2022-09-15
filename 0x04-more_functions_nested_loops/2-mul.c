877f4d650213c17e074320a2b5feb3412f12822c
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-15 02:21:21 -0700

    added 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
