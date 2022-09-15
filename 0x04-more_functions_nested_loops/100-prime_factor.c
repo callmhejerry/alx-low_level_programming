d2dcc9bb7c592674fa3c0848ba7c455c9de705cc
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-15 15:19:34 -0700

    fixed all

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,12 +9,14 @@ int main(void)
 	long x, y;
 
 	x = 612852475143;
-	for (y = 2; x > y; y++)
+	y = 2;
+	while (x > y)
 	{
 		while (x % y == 0)
 		{
 			x = x / y;
 		}
+		y++;
 	}
 	printf("%lu", y);
 	putchar('\n');
