765f23bb10ecf99728928f7759d111448d4e8c38
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-15 16:22:39 -0700

    added the 2_largest_number.c file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,8 +7,16 @@
 
 int main(void)
 {
+	int i;
 
 	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (1 < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
