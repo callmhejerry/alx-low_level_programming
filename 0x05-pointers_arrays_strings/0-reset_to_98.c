0a7c29fd0d58eb8c534a73b2e31d4f819ca45b05
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-19 04:20:49 -0700

    added a new folder on pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int as
+ * paramenter and updates the value to 98
+ * @*n: the pointer to the value to be changed
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains files relating ro pointers , arrays and memory in c programming
