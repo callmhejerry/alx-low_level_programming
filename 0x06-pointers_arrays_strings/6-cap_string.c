5393ff89633b250bd5b496837ddc0ac3b3c1eed1
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-21 12:28:21 -0700

    added 6-cap-string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * cap_string - a function that capitalizes evry word of a string
+ * @str: the string to capitalize
+ *
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		if (str[len] >= 'a' && str[len] <= 'z')
+		{
+			if (str[len - 1] == '.' || str[len - 1] == ' ')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '\t' || str[len - 1] == '\n')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == ';' || str[len - 1] == ',')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '!' || str[len - 1] == '?')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '"' || str[len - 1] == '(')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == ')' || str[len - 1] == '{')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '}')
+				str[len] = str[len] - 32;
+			len++;
+		}
+		else if (str[len] >= 'A' && str[len] <= 'Z')
+		{
+			if (str[len - 1] >= 'A' && str[len - 1] <= 'Z')
+				str[len] = str[len] + 32;
+			if (str[len - 1] >= 'a' && str[len - 1] <= 'z')
+				str[len] = str[len] + 32;
+			len++;
+		}
+		else if (str[len] == '\t')
+			str[len] = ' ';
+		else
+			len++;
+	}
+	return (str);
+}
