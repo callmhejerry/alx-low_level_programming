43a776299c533fe065bc230b0cb8f4be67d44510
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-21 08:00:39 -0700

    addded 4-rev_arry file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,8 @@
 /**
  * reverse_array - A function that reverses the content
  * of an array of integers
- * a: the array to be reversed
- * n: the number of elements of the array
+ * @a: the array to be reversed
+ * @n: the number of elements of the array
  *
  * Return: void
  */
