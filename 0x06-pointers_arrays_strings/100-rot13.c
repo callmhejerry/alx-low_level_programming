c2bdeb62a57fbe426ed1d1604d13f09a66c08ea9
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-23 06:08:09 -0700

    added 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+/**
+ * rot13 - A function that encodes a string to rot13
+ * @str: the string to be encoded
+ *
+ * Return: the encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char small[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l'
+		, 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'
+			, 'x', 'y', 'z'};
+	char cap[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'
+		, 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
+			, 'V', 'W', 'X', 'Y', 'Z'};
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (j < 26)
+		{
+			if (str[i] == small[j])
+			{
+				str[i] = small[(j + 13) % 26];
+				break;
+			}
+			else if (str[i] == cap[j])
+			{
+				str[i] = cap[(j + 13) % 26];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
