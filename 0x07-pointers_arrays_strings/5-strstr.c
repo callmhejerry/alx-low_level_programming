5636678989eea6573098ecccc2e0e17fd9ca1b5b
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-26 11:21:36 -0700

    hot fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 /**
- * _strstr -  function that finds the first occurance of the
+ * _strstr - function that finds the first occurance of the
  * substring needle int the string haystack.
  * @haystack: the string to be searched
  * @needle: the string to search for
@@ -11,23 +11,23 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, k;
 
-	 i = 0;
-	 while (haystack[i] != '\0')
-	 {
-		 j = 0;
-		 if (haystack[i] == needle[j])
-		 {
-			 k = i;
-			 while (needle[j] != '\0')
-			 {
-				 if (haystack[i] != needle[j]
-						  && haystack[i] != '\0')
-					 return (0);
-				 i++;
-				 j++;
-			 }
-			 return (haystack + k);
-		 }
-		 i++;
-	 }
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			k = i;
+			while (needle[j] != '\0')
+			{
+				if (haystack[i] != needle[j]
+						 && haystack[i] != '\0')
+					return (0);
+				i++;
+				j++;
+			}
+			return (haystack + k);
+		}
+		i++;
+	}
 }
