b450e318c04b90afd9d2c0076bf1cb73b3c73dad
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-30 06:58:28 -0700

    added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - A program that multiplies two digits
+ * @argc: the number of arguments passed
+ * @argv: the array of arguments passed
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	long int result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = argv[1] * argv[2];
+		printf("%li\n", result);
+	}
+	return (0);
+}
