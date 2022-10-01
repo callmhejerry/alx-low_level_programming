de670916c8903fcfc43027b323477d0045ea7b99
callmhejerry <chinedujeremiah2002@gmail.com>
2022-09-30 17:50:26 -0700

    hot fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
+int check(char *str);
 
 /**
  * main - A function that that prints the minimum number of coins to
@@ -45,4 +46,44 @@ int main(int argc, char *argv[])
 		printf("%i\n", i);
 	}
 	return (0);
+}
+
+/**
+ * check - checks if a character is a digit
+ * @str: the character to check
+ *
+ * Return: 0 if it is not a digit1 if itis a digit
+ */
+int check(char *str)
+{
+	int i, j;
+
+	j = 1;
+	if (str[0] == '-' && str[1] != '\0')
+	{
+		i = 1;
+		while (str[i] != '\0')
+		{
+			if (str[i] < '0' || str[i] > '9')
+			{
+				j = 0;
+				break;
+			}
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			if (str[i] < '0' || str[i] > '9')
+			{
+				j = 0;
+				break;
+			}
+			i++;
+		}
+	}
+	return (j);
 }
