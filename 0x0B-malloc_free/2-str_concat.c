a18c788b5d580cb01a2a19d2f8ab74726202adc1
callmhejerry <chinedujeremiah2002@gmail.com>
2022-10-03 04:37:17 -0700

    added 2-str_cat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,21 +12,35 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int size, i, k, a, l, m;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (k = 0; s2[k] != '\0'; k++)
-		;
-	size = i + k + 1;
+	size = 1;
+	if (s1 == NULL && s2 != NULL)
+	{
+		for (k = 0; s2[k] != '\0'; k++)
+			;
+		size = k + 1;
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			;
+		size = i + 1;
+	}
+	else if (s1 != NULL && s2 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			;
+		for (k = 0; s2[k] != '\0'; k++)
+			;
+		size = i + k + 1;
+	}
 	ptr = (char *)malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (a = 0; a < size; )
 	{
-		for (l = 0; s1[l] != '\0'; l++, a++)
+		for (l = 0; s1[l] != '\0' && s1 != NULL; l++, a++)
 			ptr[a] = s1[l];
-		for (m = 0; s2[m] != '\0'; m++, a++)
+		for (m = 0; s2[m] != '\0' && s2 != NULL; m++, a++)
 			ptr[a] = s2[m];
 		ptr[a] = '\0';
 		a++;
