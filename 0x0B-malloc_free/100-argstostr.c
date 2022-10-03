6f8bba1b3c13bdf0bfef21a5dfe2a797719491ed
callmhejerry <chinedujeremiah2002@gmail.com>
2022-10-03 09:24:52 -0700

    added 100-argstostr.c file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,10 +14,10 @@ char *argstostr(int ac, char **av)
 	char *ptr;
 	int i, j, k, count;
 
-	if (ac <= 1 || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 	count = 0;
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++, count++)
 			;
@@ -27,7 +27,7 @@ char *argstostr(int ac, char **av)
 	ptr = (char *)malloc(sizeof(char) * count);
 	for (k = 0; k < count; )
 	{
-		for (i = 1; i < ac; i++)
+		for (i = 0; i < ac; i++)
 		{
 			for (j = 0; av[i][j] != '\0'; j++)
 			{
