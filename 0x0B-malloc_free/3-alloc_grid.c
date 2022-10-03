bd31c5a66928a49bdb1df01abcfc7d99e348d677
callmhejerry <chinedujeremiah2002@gmail.com>
2022-10-03 06:16:14 -0700

    added 3-alloc_grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,7 @@
 int **alloc_grid(int width, int height)
 {
 	int *ptrarr, w, h, **arr;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	ptrarr = (int *)malloc(sizeof(int) * (width * height));
