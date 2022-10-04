43eb2bc52994d996b385d0198e3480b9d99af372
callmhejerry <chinedujeremiah2002@gmail.com>
2022-10-04 03:39:28 -0700

    added 101-strtow.c file

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+
+/**
+ * strtow - A function that splits a string into words
+ * @str: the pointer to the string
+ *
+ * Return: A pointer to the array of words
+ */
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, count, j, k;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	count = total(str);
+	ptr = (char **)malloc(sizeof(char *) * count);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0, j = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for (k = 1; str[i] != ' '; k++, i++)
+				;
+			k++;
+			if (j < count)
+			{
+				ptr[j] = (char *)malloc(sizeof(char) * k);
+				if (ptr[j] == NULL)
+					free(ptr[j]);
+				j++;
+			}
+			else
+				ptr[j] = NULL;
+		}
+	}
+	for (i = 0, j = 0; str[i] != '\0'; i++)
+		if (str[i] != ' ')
+		{
+			if (j < count)
+			{
+				for (k = 0; str[i] != ' '; k++, i++)
+					ptr[j][k] = str[i];
+				ptr[j][k] = ' ';
+				ptr[j][k + 1] = '\0';
+				j++;
+			}
+		}
+	return (ptr);
+}
+
+/**
+ * total - A function to loop through
+ * a string
+ * @str: the pointer to the string
+ * Return: the number of words in the string
+ */
+int total(char *str)
+{
+	int count, i;
+
+	for (i = 0, count = 1; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for ( ; str[i] != ' '; i++)
+				;
+			count++;
+		}
+	}
+	return (count);
+}
