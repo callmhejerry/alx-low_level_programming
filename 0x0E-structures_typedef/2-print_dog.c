d561838dced384372132f09b7afe3a6c90be0261
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-10 21:39:51 +0100

    hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nill)\n");
 	else
 		printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nill)\n");
 	else
