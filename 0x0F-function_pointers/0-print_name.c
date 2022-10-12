b625109eeb7dd961e12a87299ae1c4044ac1e8f3
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-12 10:00:33 +0100

    added 0-print_name.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - A function that takes a callback function
+ * and prints a name
+ * @name: the name to be printed
+ * @f: the callback function to call to print the name
+ * 
+ * Return: void
+ */
+void print_name(char *name, void (*f)(*char))
+{
+    f(name);
+}
