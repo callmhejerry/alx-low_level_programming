96f830aa7365316eadda0286b2ff702ae7c2a09f
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-13 14:09:25 +0100

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,11 +16,11 @@ va_list ap;
 result = 0;
 if (n == 0)
 return (0);
-va_start (ap, n);
+va_start(ap, n);
 for (i = 0; i < n; i++)
 {
 result += va_arg(ap, int);
 }
-va_end (ap);
+va_end(ap);
 return (result);
 }
