473157a880ea7b7ea5dbd515e18906f9d6d2f815
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-13 11:58:23 +0100

    started a new project

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - A function that returns the sum of all
+ * its parameter
+ * @n: the number of parameters
+ * Return: the sum of the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int result;
+va_list ap;
+
+result = 0;
+if (n == 0)
+return (0);
+va_start (ap, n);
+for (i = 0; i < n; i++)
+{
+result += va_arg(ap, int);
+}
+va_end (ap);
+return (result);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+A new topic on variadic function
