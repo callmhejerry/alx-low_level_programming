ad36371f40a5182928b0b201437ce70359688570
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-25 13:06:02 +0100

    Added 101-print_safe file

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * print_listint_safe - A function that prints a listint linked list
+  * @head: the pointer to the first node of the list
+  * Return: the size of the list
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *before, *after;
+	size_t count;
+
+	if (head == NULL)
+		exit(98);
+	before = head;
+	after = head;
+	count = 0;
+	while (before >= after && after != NULL)
+	{
+		printf("[%p] %i\n", (void *)after, after->n);
+		before = after;
+		after = after->next;
+		count++;
+	}
+	if (after != NULL)
+		printf("-> [%p] %i\n", (void *)after, after->n);
+	return (count);
+}
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	node = (listint_t *)malloc(sizeof(listint_t));
 	if (node == NULL)
+	{
+		free(node);
 		return (NULL);
+	}
 	node->n = n;
 	if (*head == NULL)
 	{
