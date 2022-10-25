218db3a66d910eda453f39592d575ba8498e7698
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-25 01:29:06 +0100

    hot fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int count;
 
 	if (*head == NULL)
-		return(-1);
+		return (-1);
 	temp = *head;
 	count = 0;
 	hold = *head;
