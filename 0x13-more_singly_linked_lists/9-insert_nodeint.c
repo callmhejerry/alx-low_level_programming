41416e7e61ef762a726fa3045243bd1fc8d7d982
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-25 02:21:38 +0100

    hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	h = *head;
 	count = 0;
-	while (temp != NULL)
+	while (head != NULL)
 	{
 		if (count == idx)
 		{
@@ -40,7 +40,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		count++;
 		h = temp;
-		temp = temp->next;
+		if (temp != NULL)
+			temp = temp->next;
 	}
 	return (NULL);
 }
