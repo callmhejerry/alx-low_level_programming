d07b1f9ecf4be70daafc52cb4f7e86e11dea68e8
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-28 21:02:25 +0100

    added 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,12 +9,13 @@
   */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int result, count, xor;
+	unsigned int result, xor;
+	unsigned long int count;
 
 	result = 0;
 	count = 0;
 	xor = n ^ m;
-	while (count <= (sizeof(unsigned long int) * 8 - 1))
+	while (count < (sizeof(unsigned long int) * 8 - 1))
 	{
 		if (xor & 1)
 			result++;
