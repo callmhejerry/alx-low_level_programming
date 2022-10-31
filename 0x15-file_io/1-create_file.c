fb9983984eb2c75f518455e1b1fde01034769fa9
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-10-31 20:17:37 +0100

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd_file = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
-	if (fd_file < 0)
-		return (-1);
+	fd_file = open(filename, O_WRONLY);
+	if (fd_file > 0)
+		truncate(filename, 0);
+	else
+	{
+		fd_file = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd_file < 0)
+			return (-1);
+	}
 	if (text_content == NULL)
 	{
 		close(fd_file);
