06e7d190f99f9d088939f23108084c1e2eae9cd8
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-23 22:38:30 +0100

    Added 5-island_perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+'''A function that calculates the perimeter of an island'''
+
+
+def island_perimeter(grid):
+    '''Returns the perimeter of the island'''
+    if type(grid) is list and type(grid[0]) is list:
+        perimeter = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == 1:
+                    perimeter = perimeter + check_sides(row, col, grid)
+        return perimeter
+
+def check_sides(row, col, grid):
+    '''check the surrounding of a land in the grid'''
+    perimeter = 0
+    # check left
+    if grid[row][col - 1] == 0:
+        perimeter = perimeter + 1
+    # check up
+    if grid[row - 1][col] == 0:
+        perimeter = perimeter + 1
+    # check right
+    if grid[row][col + 1] == 0:
+        perimeter = perimeter + 1
+    # check down
+    if grid[row + 1][col] == 0:
+        perimeter = perimeter + 1
+    
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
