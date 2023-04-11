5949285a4eb3890d13c4d24062a82b6a59a5da4f
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-11 15:38:56 +0100

    started a new project on searching algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - A function that searches for a value
+ * in an array of integers using the linear search algorithm
+ * @array: the pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: -1 if the array is NULL or value not found, else
+ * return the first index where the value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,75 @@
+#include "search_algos.h"
+
+void print_search(size_t start, size_t end, int *array);
+int recur_search(int *array, size_t start, size_t end, int value);
+
+/**
+ * binary_search - A function that searches for a value in a
+ * sorted array of integers using the binary search algorithm
+ * @array: the pointer to the first element of the array to
+ * search for
+ * @size: the number of elements in the array
+ * @value: the value to search for
+ * Return: the index where value is located, else return -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (recur_search(array, 0, size, value));
+}
+
+/**
+ * print_search - A function that prints "searching in array"
+ * when the binaray search function is searching
+ * @start: the start of the array
+ * @end: the end of the array
+ * @array: the pointer to the array
+ * Return: void
+ */
+void print_search(size_t start, size_t end, int *array)
+{
+	size_t i;
+
+	printf("Searching in array:");
+	for (i = start; i < end; i++)
+	{
+		if (i + 1 == end)
+			printf(" %i\n", array[i]);
+		else
+			printf(" %i,", array[i]);
+	}
+}
+
+
+/**
+ * recur_search - A function that searches for a number
+ * recursively
+ * @array: the array to search
+ * @start: the start of the array
+ * @end: the end of the array
+ * @value: the value to search for
+ * Return: the index where the value is located, else -1
+ */
+int recur_search(int *array, size_t start, size_t end, int value)
+{
+	size_t mid, check;
+	int mid_val;
+
+	check = end - start;
+
+	if (check == 1)
+		mid = start;
+	else
+		mid = ((start + end) / 2) - 1;
+	mid_val = array[mid];
+
+	print_search(start, end, array);
+	if (mid_val == value)
+		return (mid);
+	if (check == 1)
+		return (-1);
+	if (value > mid_val)
+		return (recur_search(array, mid + 1, end, value));
+	return (recur_search(array, start, mid, value));
+}
